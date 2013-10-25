7a3cab053f0ad2bf1e92ffcc822e7579ee8f8c46
Florian Leitner <florian.leitner@gmail.com>
2013-10-25 17:13:32 +0200

    version, man and README

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name='medic',
-    version='1.1.0',
+    version='1.1.1',
     license='GNU GPL v3',
     author='Florian Leitner',
     author_email='florian.leitner@gmail.com',
