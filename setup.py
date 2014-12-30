f80f1be23fd912f16aa96b4af1a71589e5c46645
Florian Leitner <florian.leitner@gmail.com>
2014-12-30 14:47:24 +0100

    Bump version: 2.1.7 → 2.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name='medic',
-    version='2.1.7',
+    version='2.2.0',
     license='GNU GPL v3',
     author='Florian Leitner',
     author_email='florian.leitner@gmail.com',
